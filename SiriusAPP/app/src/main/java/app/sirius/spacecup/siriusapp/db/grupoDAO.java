99499f4197b494fe4f86df77731d7444b9dcfbc5
Gabriel Ribeiro <gabrielltr84@gmail.com>
2015-10-17 14:18:08 -0300

    Entidade de dados Grupo

--- SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/db/grupoDAO.java
+++ SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/db/grupoDAO.java
@@ -5,7 +5,7 @@ import android.content.ContentValues;
 /**
  * Created by Gabriel on 17/10/2015.
  */
-public class GrupoDAO extends DAOObject<Grupo> {
+public class GrupoDAO extends DAOObject<GrupoDAO.Grupo> {
 
     @Override
     public ContentValues getContentValues() {
@@ -30,4 +30,46 @@ public class GrupoDAO extends DAOObject<Grupo> {
     }
 
 
+    /**
+     * Classe de objeto para acesso aos dados
+     */
+    public static class Grupo {
+        private int _id;
+        private String nome_grupo;
+        private String nome_turma;
+
+        public Grupo() {
+        }
+
+        public Grupo(int _id, String nome_grupo, String nome_turma) {
+            this._id = _id;
+            this.nome_grupo = nome_grupo;
+            this.nome_turma = nome_turma;
+        }
+
+        public int get_id() {
+            return _id;
+        }
+
+        public void set_id(int _id) {
+            this._id = _id;
+        }
+
+        public String getNome_grupo() {
+            return nome_grupo;
+        }
+
+        public void setNome_grupo(String nome_grupo) {
+            this.nome_grupo = nome_grupo;
+        }
+
+        public String getNome_turma() {
+            return nome_turma;
+        }
+
+        public void setNome_turma(String nome_turma) {
+            this.nome_turma = nome_turma;
+        }
+
+    }
 }
