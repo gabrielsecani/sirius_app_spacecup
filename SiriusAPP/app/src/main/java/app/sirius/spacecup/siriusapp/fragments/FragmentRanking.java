da8b20d9c39a8e49aaa728cb414d89db94f5c729
Fernando Seguim <fernando.seguim@gmail.com>
2015-10-18 02:44:02 -0200

    Blá Blá Blá

--- SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/fragments/FragmentRanking.java
+++ SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/fragments/FragmentRanking.java
@@ -2,6 +2,9 @@ package app.sirius.spacecup.siriusapp.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +14,16 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.melnykov.fab.FloatingActionButton;
+
 import java.util.List;
 import java.util.Map;
 
 import app.sirius.spacecup.siriusapp.R;
 import app.sirius.spacecup.siriusapp.db.RankingDAO;
 
-
 public class FragmentRanking extends FragmentBase {
 
-
-    /*private List<Map<String, Object>> grupos;*/
     private ListView listView;
 
     public FragmentRanking(){
@@ -45,12 +47,9 @@ public class FragmentRanking extends FragmentBase {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle SaveInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle SaveInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_ranking, container, false);
-
-        String[] chaves = {"grupo", "distancia", "posicao", "img"};
-        int[] identificadores = {R.id.txtView_nome_grupo, R.id.txtView_distancia_alvo, R.id.txtView_rank_position, R.id.icon_rankin_position};
 
         listView = (ListView) view.findViewById(R.id.listView_ranking);
 
@@ -61,6 +60,9 @@ public class FragmentRanking extends FragmentBase {
             ((TextView) view.findViewById(R.id.txtView_sem_grupos)).setVisibility(View.VISIBLE);
             listView.setVisibility(View.GONE);
         } else {
+
+            String[] chaves = {"grupo", "distancia", "posicao", "img"};
+            int[] identificadores = {R.id.txtView_nome_grupo, R.id.txtView_distancia_alvo, R.id.txtView_rank_position, R.id.icon_rankin_position};
 
             SimpleAdapter adapter =
                     new SimpleAdapter(getContext(), grupos,
@@ -83,14 +85,31 @@ public class FragmentRanking extends FragmentBase {
             });
 
         }
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Toast.makeText(getContext(), "TESTE",
+                        Toast.LENGTH_SHORT).show();*/
+
+                try {
+                    FragmentBase fragment = FragmentCadNovoGrupo.newInstance(null, null);
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout_menu, fragment);
+                    fragmentTransaction.commit();
+                    /*android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) view.findViewById(R.id.toolbar_menu);
+                    ((TextView) toolbar.findViewById(R.id.txtToolbarDescricao)).setText(getResources().getString(R.string.toolbar_title_NovoGrupo));*/
+
+                } catch (ClassCastException e) {
+                    throw new ClassCastException(FragmentCadNovoGrupo.newInstance(null, null).toString() + " must extend FragmentBase");
+                } catch (Exception ex) {
+                    Log.e("setFragment", ex.getMessage());
+                }
             }
-        });*/
+
+        });
 
         return view;
     }
