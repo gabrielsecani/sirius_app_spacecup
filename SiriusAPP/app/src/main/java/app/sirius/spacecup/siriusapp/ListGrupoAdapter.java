3499eb81fd97bacf81c4ab604e4b19b5f895b4d1
Gabriel Ribeiro <gabrielltr84@gmail.com>
2015-10-21 16:18:53 -0200

    &@$%# Spiiinner de GRUPO funcionando com List..Adapter buscando direto dados do objeto de banco (DAO)

--- SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/ListGrupoAdapter.java
+++ SiriusAPP/app/src/main/java/app/sirius/spacecup/siriusapp/ListGrupoAdapter.java
@@ -15,40 +15,43 @@ import app.sirius.spacecup.siriusapp.db.GrupoDAO;
  * Created by gabriel.ribeiro on 20/10/2015.
  */
 public class ListGrupoAdapter extends BaseAdapter {
-    private List<GrupoDAO.Grupo> grupos;
-    private LayoutInflater inflater = null;
+    private List<GrupoDAO.Grupo> mGrupos;
+    private LayoutInflater mInflater = null;
 
     ListGrupoAdapter() {
     }
 
     public ListGrupoAdapter(Context context, List<GrupoDAO.Grupo> grupoList) {
-        this.grupos = grupoList;
-        this.inflater = LayoutInflater.from(context);
+        this.mGrupos = grupoList;
+//        this.mInflater = LayoutInflater.from(context);
+        this.mInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return grupos.size();
+        return mGrupos.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return grupos.get(position);
+        return mGrupos.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return grupos.get(position).get_id();
+        return mGrupos.get(position).get_id();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = inflater.inflate(R.layout.layout_lista_grupo, null);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.layout_lista_grupo, null);
+        }
         TextView textView;
-        textView = (TextView) view.findViewById(R.id.layout_lista_grupo_nome_grupo);
-        textView.setText(grupos.get(position).getNome_grupo());
-        textView = (TextView) view.findViewById(R.id.layout_lista_grupo_turma);
-        textView.setText(grupos.get(position).getNome_turma());
-        return view;
+        textView = (TextView) convertView.findViewById(R.id.layout_lista_grupo_nome_grupo);
+        textView.setText(mGrupos.get(position).getNome_grupo());
+        textView = (TextView) convertView.findViewById(R.id.layout_lista_grupo_turma);
+        textView.setText(mGrupos.get(position).getNome_turma());
+        return convertView;
     }
 }
